f2d03eebca4630f394a7d9ea2addd86ed7bc1928
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-10 13:20:13 +0100

    Se actualizan las importaciones de los componentes para utilizarse. Se elimina código innecesario en ' app.component.html'.

--- angularEjemplos/primerangular/src/app/app.module.ts
+++ angularEjemplos/primerangular/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import {PrimerComponente} from "./../components/primercomponente/PrimerComponente.component";
 import {CiclosVida} from "./../components/ciclosvida/ciclosvida.component"
 import {Deportes} from "./../components/deportes/deportes.component"
+import {NumerosRandom} from "./../components/numerosRandom/numerosrandom.component"
 @NgModule({
   declarations: [
-    AppComponent, PrimerComponente,CiclosVida, Deportes
+    AppComponent, PrimerComponente,CiclosVida, Deportes, NumerosRandom
   ],
   imports: [
     BrowserModule
---
+++ angularEjemplos/primerangular/src/components/numerosRandom/numerosrandom.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-numeros-random",
+    templateUrl: "./numerosrandom.component.html"
+})
+
+export class NumerosRandom {
+    
+}
