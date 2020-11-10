06bd0e4b10c5e191d3ad61139e364d1147f46f54
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-10 13:14:47 +0100

    cambios en un deporte del array

--- angularEjemplos/primerangular/src/components/deportes/deportes.component.ts
+++ angularEjemplos/primerangular/src/components/deportes/deportes.component.ts
@@ -6,6 +6,6 @@ templateUrl: "./deportes.component.html",
 export class Deportes {
     public sports: Array<string>
     constructor(){
-        this.sports = ["Tiro olímpico", "baloncesto", "rally", "fútbol", "cricket", "golf", "curling", "surf"];
+        this.sports = ["Tiro olímpico", "baloncesto", "rally", "fútbol", "MTB", "golf", "curling", "surf"];
     }
 }
