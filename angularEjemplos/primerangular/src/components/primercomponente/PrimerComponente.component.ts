97a33ba4bd719814f416441f9c1cfaeda5890353
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-10 11:05:35 +0100

    Se añaden variables y se crea 'PrimerComponente.component.html'

--- angularEjemplos/primerangular/src/components/primercomponente/PrimerComponente.component.ts
+++ angularEjemplos/primerangular/src/components/primercomponente/PrimerComponente.component.ts
@@ -5,13 +5,20 @@ import {Component} from '@angular/core';
     //Nombre del componente para la app
     selector: "app-primercomponente",
     // plantilla para el componente
-    template: `<h1>Soy el primer componente Angular </h1>`
+    template: `<h1>Soy el primer componente Angular </h1>
+    <h2>{{nombre}}</h2><h2>{{anyo}}</h2>`
 })
 
 //Nombre par ala declaración en MODULE
 export class PrimerComponente{
+    //declarar variables con tipo
+    public nombre: string;
+    public anyo: number;
     //La primera vez que entra en el componente tenemos un constructor
     constructor(){
     console.log("creando el primer componente");
+    //con this accedemos a las propiedades
+    this.nombre = "S*ck my d*ck";
+    this.anyo = 2020;
     }
 }
