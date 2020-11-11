666e05717c3380778272f64a5c27e08daaf4da3a
Mario <canalessanchezmario@gmail.com>
2020-11-11 13:54:11 +0100

    Se termina el desarrollo de 'tablasmultiplicar'. Se crea la carpeta 'rutas' y todos sus componentes(home, cine, television, musica) dentro.

--- rutasangular/src/app/app.module.ts
+++ rutasangular/src/app/app.module.ts
@@ -4,8 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TablasMultiplicar } from './../components/tablasmultiplicar/tablasmultiplicar.component';
 import { FormsModule } from '@angular/forms';
+//import para utilizar las rutas
+import { Route } from '@angular/router';
+import { MusicaComponent } from './musica/musica.component';
+import { HomeComponent } from './components/home/home.component';
+import { CineComponent } from './components/cine/cine.component';
+import { TelevisionComponent } from './components/television/television.component';
 @NgModule({
-  declarations: [AppComponent, TablasMultiplicar],
+  declarations: [AppComponent, TablasMultiplicar, MusicaComponent, HomeComponent, CineComponent, TelevisionComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
