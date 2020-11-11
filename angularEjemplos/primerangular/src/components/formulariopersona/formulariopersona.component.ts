adfcacdf97e951f6f55f860fdd460089b0d096a4
Mario <canalessanchezmario@gmail.com>
2020-11-11 09:38:09 +0100

    Se crea un nuevo componente para los formularios

---
+++ angularEjemplos/primerangular/src/components/formulariopersona/formulariopersona.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulariopersona',
+  templateUrl: './formulariopersona.component.html',
+  styleUrls: ['./formulariopersona.component.css']
+})
+export class FormulariopersonaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
