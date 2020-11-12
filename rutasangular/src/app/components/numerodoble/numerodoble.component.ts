404fa21b87d76a877b3bb3f91caf14442dc116b5
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-12 10:54:32 +0100

    Se añade lógica y se muestra por pantalla con

--- rutasangular/src/app/components/numerodoble/numerodoble.component.ts
+++ rutasangular/src/app/components/numerodoble/numerodoble.component.ts
@@ -7,6 +7,8 @@ import {ActivatedRoute, Router, Params} from "@angular/router";
   styleUrls: ['./numerodoble.component.css']
 })
 export class NumerodobleComponent implements OnInit {
+  public doble: number;
+  public numero: number;
   //Se tienen que inyectar los dos objetos en el contructor para poder recuperarlos
   //En la inyeccion se declaran con private. CUando son inyectadas llevan _
   constructor( private _activedRoute: ActivatedRoute, private _router:Router ) { 
@@ -17,7 +19,14 @@ export class NumerodobleComponent implements OnInit {
      //Debemos suscribirnos para recibirlo
     //Vendran sus parametros con su :name
     this._activedRoute.params.subscribe((params: Params)=>{
-      console.log(params)
+      //Se han creado rutas sin numero, comprobamos que existe parametro
+      if(params.numero!=null){
+        //Los parametros son string aunque sean númericos
+        this.numero = parseInt(params.numero);
+        this.doble = this.numero*2;
+      } else {
+        console.log('No params')
+      }
     });
   }
 
