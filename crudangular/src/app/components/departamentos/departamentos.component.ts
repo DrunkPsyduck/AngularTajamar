0c22a23534a66e5a7ebf7b16cd2f3d28ac4a069e
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-18 11:32:16 +0100

    Se añade código HTML para mostrar el contenido de un array.

--- crudangular/src/app/components/departamentos/departamentos.component.ts
+++ crudangular/src/app/components/departamentos/departamentos.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Departamento } from 'src/app/models/departamento';
+import { DepartamentoService } from 'src/app/services/departamento.service';
 
 @Component({
   selector: 'app-departamentos',
   templateUrl: './departamentos.component.html',
-  styleUrls: ['./departamentos.component.css']
+  styleUrls: ['./departamentos.component.css'],
 })
 export class DepartamentosComponent implements OnInit {
-
-  constructor() { }
+  public departamentos: Array<Departamento>;
+  constructor(private _service: DepartamentoService) {}
 
   ngOnInit(): void {
+    this._service.getDepartamentos().subscribe((response) => {
+      this.departamentos = response;
+    });
   }
-
 }
