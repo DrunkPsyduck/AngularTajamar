98b4f2c41a612cc3f15a0f58a08ada977ec5173b
Mario Canales <Mario.Canales@tajamar365.com>
2020-11-18 11:47:11 +0100

    Se crea el componente 'insertar'. Se modifican los enlaces para que utilizen '[routerLink]'. Se continua el desarrollo.

--- crudangular/src/app/app.module.ts
+++ crudangular/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { DepartamentoService } from './services/departamento.service';
 import { MenuComponent } from './components/menu/menu.component';
 import { DepartamentosComponent } from './components/departamentos/departamentos.component';
+import { InsertarComponent } from './components/insertar/insertar.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DepartamentosComponent],
+  declarations: [AppComponent, MenuComponent, DepartamentosComponent, InsertarComponent],
   imports: [BrowserModule, FormsModule, routing, HttpClientModule],
   providers: [appRoutingProvider, DepartamentoService],
   bootstrap: [AppComponent],
--- crudangular/src/app/app.routing.ts
+++ crudangular/src/app/app.routing.ts
@@ -1,8 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DepartamentosComponent } from './components/departamentos/departamentos.component';
+import { InsertarComponent } from './components/insertar/insertar.component';
 
-const appRoutes: Routes = [{ path: '', component: DepartamentosComponent }];
+const appRoutes: Routes = [
+  { path: '', component: DepartamentosComponent },
+  { path: 'insertar', component: InsertarComponent },
+];
 
 export const appRoutingProvider: any[] = [];
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(
